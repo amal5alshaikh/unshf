24f686aefda9307975a3bc0ab87dde52ccfbe7dc
amal5alshaikh <amal515@hotmail.com>
2021-09-14 19:15:31 +0300

    table

---
+++ unshf/src/app/hero-table/hero-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-table',
+  templateUrl: './hero-table.component.html',
+  styleUrls: ['./hero-table.component.sass']
+})
+export class HeroTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
